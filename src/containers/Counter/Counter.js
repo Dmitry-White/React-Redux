f7d84dd76f976482746d9be15248090785ad65b4
Dmitry <ecio1476@gmail.com>
2018-03-12 13:43:43 +0800

    Add store

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -6,7 +7,7 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 class Counter extends Component {
     state = {
         counter: 0
-    }
+    };
 
     counterChangedHandler = ( action, value ) => {
         switch ( action ) {
@@ -25,19 +26,25 @@ class Counter extends Component {
             default:
                 break;
         }
-    }
+    };
 
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
+                <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
                 <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
             </div>
         );
+    };
+};
+
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
     }
 }
 
-export default Counter;
+export default connect(mapStateToProps)(Counter);
